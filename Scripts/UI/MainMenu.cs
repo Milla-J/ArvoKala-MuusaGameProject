c888a8970740587c7a3e2683e6674da3de674c32
KalleKarhu <kallekarhu20@gmail.com>
2026-03-06 22:11:20 +0200

    Added comments to MainMenu.cs

--- Scripts/UI/MainMenu.cs
+++ Scripts/UI/MainMenu.cs
@@ -3,33 +3,27 @@ using System;
 
 public partial class MainMenu : Control
 {
-	
-	public override void _Ready()
-	{	
-	}
+	//When button pressed: calls the game scene
 	private void OnPlayPressed()
 	{
 		GD.Print("Play pressed");
 		GetTree().ChangeSceneToFile("res://Scenes/Game.tscn");
-
 	}
 
-	
+	//When button pressed: calls the settings menu scene
 	private void OnSettingsPressed()
 	{
 		GD.Print("Settings pressed");
 		GetTree().ChangeSceneToFile("Scenes/UI/settings_menu.tscn");
-
 	}
-
+    //When button pressed: calls the credits menu scene
 	private void OnCreditsPressed()
 	{
 		GD.Print("Credits pressed");
-		GetTree().ChangeSceneToFile("res://Scenes/UI/credits_menu.tscn");
-		
-		
+		GetTree().ChangeSceneToFile("res://Scenes/UI/credits_menu.tscn");	
 	}
 
+    //When button pressed: quits the game
 	private void OnQuitPressed()
 	{
 		GD.Print("Quit pressed");
