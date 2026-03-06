e4d3493f05fc8416b2d49cdb5a44cdeec16980c8
Milla-J <milla.jerohin@gmail.com>
2026-03-06 09:23:21 +0200

    Updated fish spawn logic

--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -6,23 +6,23 @@ public partial class GameController : Node
 {
 	[ExportCategory("References")]
 	[Export] private Minigame _minigame;
-    [Export] private PackedScene _fishScene;
     [Export] private bool _testBool = false;
     [Export] private Node2D _hook;
     [Export] private Control _spawnArea;
 
     [ExportCategory("Fish lists")]
-    [Export] private PackedScene[] _fishScenes = new PackedScene[2];
+    [Export] private PackedScene[] _fishPool;
+    private List<Fish> _spawnedFish = new List<Fish>();
+    private int _fishPoolCount;
 
     private Rect2 _screenRect;
-
-    private List<Fish> _fishies = new List<Fish>();
 
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
     {
+        _fishPoolCount = _fishPool.Length;
         _screenRect = _spawnArea.GetRect();
-        SpawnFish(10);
+        SpawnFish(5);
     }
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -30,7 +30,7 @@ public partial class GameController : Node
     {
         if(_testBool)
         {
-            _fishies[GD.RandRange(0, _fishies.Count - 1)]._isTargeting = true;
+            _spawnedFish[GD.RandRange(0, _spawnedFish.Count - 1)]._isTargeting = true;
             _testBool = false;
         }
     }
@@ -43,19 +43,50 @@ public partial class GameController : Node
 
     public void SpawnFish(int fishAmount)
     {
+        if (fishAmount > _fishPoolCount)
+        {
+            GD.PrintErr("Attempted to spawn too many fish");
+            return;
+        }
+
         for (int i = 0; i < fishAmount; i++)
         {
+            int index = GD.RandRange(0, _fishPoolCount - 1);
+            GD.Print($"{index} : {_fishPoolCount}");
             // spawn the fish and add it to the fishies list
-            //Fish fish = _fishScene.Instantiate<Fish>();
-            Fish fish = _fishScenes[GD.RandRange(0, _fishScenes.Length - 1)].Instantiate<Fish>();
+            Fish fish = _fishPool[index].Instantiate<Fish>();
+            RemoveFishFromPool(index);
+
             fish.SetTarget(_hook);
             AddChild(fish);
-            _fishies.Add(fish);
+            _spawnedFish.Add(fish);
 
             // set the fishes position to a random spot on the screen
             int horizontalPosition = (int)GD.RandRange(_screenRect.Position.X, _screenRect.End.X);
             int verticalPosition = (int)GD.RandRange(_screenRect.Position.Y, _screenRect.End.Y);
             fish.GlobalPosition = new Vector2(horizontalPosition, verticalPosition);
         }
+    }
+
+    private void RemoveFishFromPool(int index)
+    {
+        if (_fishPoolCount <= 0)
+        {
+            GD.PrintErr("Fish pool empty");
+            return;
+        }
+        _fishPoolCount--;
+        PackedScene[] tempArray = new PackedScene[_fishPoolCount];
+        int newIndex = 0;
+        for (int i = 0; i < _fishPool.Length; i++)
+        {
+            if (i != index)
+            {
+                tempArray[newIndex] = _fishPool[i];
+                newIndex++;
+            }
+        }
+        _fishPool = tempArray;
+        GD.Print(_fishPool);
     }
 }
