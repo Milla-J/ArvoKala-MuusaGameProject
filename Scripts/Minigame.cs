49940cc3638b22ddc3198e2821f3ec67ef0853fc
Milla-J <milla.jerohin@gmail.com>
2026-03-06 12:25:17 +0200

    Updated minigame

--- Scripts/Minigame.cs
+++ Scripts/Minigame.cs
@@ -1,17 +1,34 @@
 using Godot;
 using System;
+using System.IO;
 
-public partial class Minigame : Node2D
+public partial class Minigame : Sprite2D
 {
     [Export] private Sprite2D _indicator;
     [Export] private bool _testBool;
 	private bool _minigameGoing;
 
+    private float _rightEdge;
+    private float _leftEdge;
+
+    // Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+    {
+        Rect2 area = GetRect();
+        _leftEdge = area.Position.X;
+        _rightEdge = area.End.X;
+        GD.Print($"{_leftEdge} : {_rightEdge}");
+    }
+
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 movementOffset = new Vector2(Input.GetAccelerometer().X, 0);
-        _indicator.Translate(movementOffset);
+        if (movementOffset.X < 0 && _indicator.Position.X > _leftEdge ||
+            movementOffset.X > 0 && _indicator.Position.X < _rightEdge)
+        {
+            _indicator.Translate(movementOffset);
+        }
 	}
 
 	public void StartMinigame()
