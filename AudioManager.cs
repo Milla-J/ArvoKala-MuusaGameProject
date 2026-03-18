e602320c38463ecca514473cb0c95b727375238c
KalleKarhu <kallekarhu20@gmail.com>
2026-03-18 20:20:59 +0200

    Added low pass filter effect to ambience when in pause menu

--- AudioManager.cs
+++ AudioManager.cs
@@ -7,22 +7,29 @@ public partial class AudioManager : Node
 	[Export] private AudioStreamPlayer _mainGameMusic;
 
 	private int _musicBus;
+	private int _sfxBus;
     private AudioEffectLowPassFilter _lowPass;
+	private AudioEffectLowPassFilter _lowPass2;
 	 private AudioEffectReverb _reverb;
 	
 
 	public override void _Ready()
 	{
     	_musicBus = AudioServer.GetBusIndex("Music");
+		_sfxBus = AudioServer.GetBusIndex("SFX");
 
         _lowPass = (AudioEffectLowPassFilter)AudioServer.GetBusEffect(_musicBus, 0);
+		_lowPass2 = (AudioEffectLowPassFilter)AudioServer.GetBusEffect(_sfxBus, 0);
 		_reverb = (AudioEffectReverb)AudioServer.GetBusEffect(_musicBus, 1);
 
         // Start in "normal gameplay" state
         _lowPass.CutoffHz = 20000.0f;
+		_lowPass2.CutoffHz = 20000.0f;
 		_reverb.Wet = 0.0f;
         AudioServer.SetBusEffectEnabled(_musicBus, 0, true);   // keep LPF on
         AudioServer.SetBusEffectEnabled(_musicBus, 1, true);  //turn reverb on
+
+		AudioServer.SetBusEffectEnabled(_sfxBus, 0, true);   // keep LPF on
 	}
 
 	public void PlayMenuMusic()
@@ -50,7 +57,9 @@ public partial class AudioManager : Node
 		if (paused)
 		{
 			// Gradually muffle music
-			tween.TweenProperty(_lowPass, "cutoff_hz", 1200.0f, 0.3f);
+			tween.TweenProperty(_lowPass, "cutoff_hz", 1500.0f, 0.3f);
+
+			tween.TweenProperty(_lowPass2, "cutoff_hz", 1500.0f, 0.3f);
 
 			// Gradually increase reverb
 			tween.TweenProperty(_reverb, "wet", 0.55f, 0.7f);
@@ -59,6 +68,8 @@ public partial class AudioManager : Node
 		{
 			// Raise the frequencies gradually back to normal
 			tween.TweenProperty(_lowPass, "cutoff_hz", 20000.0f, 0.7f);
+
+			tween.TweenProperty(_lowPass2, "cutoff_hz", 20000.0f, 0.7f);
 
 			// Fade reverb back out
 			tween.TweenProperty(_reverb, "wet", 0.0f, 0.2f);
