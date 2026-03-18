ee82417291b093cc111339016830a31d0c39000f
KalleKarhu <kallekarhu20@gmail.com>
2026-03-18 18:41:25 +0200

    Made music and SFX mutable in the settings, added nature ambiance track

--- Scripts/UI/SettingsMenu.cs
+++ Scripts/UI/SettingsMenu.cs
@@ -3,19 +3,40 @@ using System;
 
 public partial class SettingsMenu : Control
 {
+	[Export] private Button _muteMusic;
+	[Export] private Button _muteSFX;
+	
 
 	private void OnMuteMusicPressed()
 	{
 		GD.Print("Mute music Pressed");
 		var _musicBus = AudioServer.GetBusIndex("Music");
-		AudioServer.SetBusMute(_musicBus, true);
+		AudioServer.SetBusMute(_musicBus, !AudioServer.IsBusMute(_musicBus));
+
+		if (AudioServer.IsBusMute(_musicBus))
+		{
+			_muteMusic.Text = "OFF";
+		}
+		else
+		{
+			_muteMusic.Text = "ON";	
+		}
 	}
 
 	private void OnMuteSFXPressed()
 	{
 		GD.Print("Mute music Pressed");
-		var _sfxBus = AudioServer.GetBusIndex("Music");
-		AudioServer.SetBusMute(_sfxBus, true);
+		var _sfxBus = AudioServer.GetBusIndex("SFX");
+		AudioServer.SetBusMute(_sfxBus, !AudioServer.IsBusMute(_sfxBus));
+
+		if (AudioServer.IsBusMute(_sfxBus))
+		{
+			_muteSFX.Text = "OFF";
+		}
+		else
+		{
+			_muteSFX.Text = "ON";	
+		}
 	}
 
 
