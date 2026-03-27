030ac2fed2cff1bcfc229e0317ef356ed9f6741e
Milla-J <milla.jerohin@gmail.com>
2026-03-27 11:49:48 +0200

    Added variables for english translations to Fish.cs

--- Scripts/Fish/Fish.cs
+++ Scripts/Fish/Fish.cs
@@ -5,7 +5,9 @@ public partial class Fish : RigidBody2D
 {
 	[ExportCategory("Public veriables")]
 	[Export] public string ValueName;
-	[Export] public string ValueDescription;
+    [Export] private string _valueNameEng;
+	[Export (PropertyHint.MultilineText)] public string ValueDescription;
+    [Export (PropertyHint.MultilineText)] private string _valueDescriptionEng;
 
 	[ExportCategory("Movement veriables")]
 	[Export] private float _speed = 150; // how fast/far movements are
--- Scripts/GameController.cs
+++ Scripts/GameController.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 
 public partial class GameController : Node
 {
-    //Signals
     [Signal]
     public delegate void CastLineEventHandler();
 
