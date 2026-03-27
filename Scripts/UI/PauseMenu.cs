e9f4813836d55533cb18eecafc4b26596176c6f5
KalleKarhu <kallekarhu20@gmail.com>
2026-03-27 17:39:16 +0200

    remade pause menu

--- Scripts/UI/PauseMenu.cs
+++ Scripts/UI/PauseMenu.cs
@@ -3,11 +3,36 @@ using System;
 
 public partial class PauseMenu : Control
 {
+	[Export] private TextureButton _muteMusic;
+	[Export] private TextureButton _muteSFX;
+	[Export] private Texture2D _onTexture;
+	[Export] private Texture2D _offTexture;
+
 	[Export] private Button _pauseButton;
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
 		 Visible = false;
+
+		 var _musicBus = AudioServer.GetBusIndex("Music");
+		if (AudioServer.IsBusMute(_musicBus))
+		{
+			_muteMusic.TextureNormal = _offTexture;
+		}
+		else
+		{
+			_muteMusic.TextureNormal = _onTexture;
+		}
+
+		var _sfxBus = AudioServer.GetBusIndex("SFX");
+		if (AudioServer.IsBusMute(_sfxBus))
+		{
+			_muteSFX.TextureNormal = _offTexture;
+		}
+		else
+		{
+			_muteSFX.TextureNormal = _onTexture;
+		}
 	}
 
 	private void OnResumePressed()
@@ -20,10 +45,38 @@ public partial class PauseMenu : Control
 		GetNode<AudioManager>("/root/AudioManager").SetPausedAudio(false);
 	}
 
-	private void OnSettingsPressed()
+	private void OnMuteMusicPressed()
 	{
-		GD.Print("Settings pressed");	
+		GD.Print("Mute music Pressed");
+		var _musicBus = AudioServer.GetBusIndex("Music");
+		AudioServer.SetBusMute(_musicBus, !AudioServer.IsBusMute(_musicBus));
+
+		if (AudioServer.IsBusMute(_musicBus))
+		{
+			_muteMusic.TextureNormal = _offTexture;
+		}
+		else
+		{
+			_muteMusic.TextureNormal = _onTexture;
+		}
 	}
+
+	private void OnMuteSFXPressed()
+	{
+		GD.Print("Mute music Pressed");
+		var _sfxBus = AudioServer.GetBusIndex("SFX");
+		AudioServer.SetBusMute(_sfxBus, !AudioServer.IsBusMute(_sfxBus));
+
+		if (AudioServer.IsBusMute(_sfxBus))
+		{
+			_muteSFX.TextureNormal = _offTexture;
+		}
+		else
+		{
+			_muteSFX.TextureNormal = _onTexture;
+		}
+	}
+
 
 	private async void OnQuitPressed()
 	{
